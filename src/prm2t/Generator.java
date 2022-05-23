5db4a74a969d81f198199d14739afbcde2e7d784
Adam Kordziukiewicz <01169305@pw.edu.pl>
2022-05-23 21:20:58 +0200

    dodanie funkcji generującej plansze, wymaga dokończenia

--- src/prm2t/Generator.java
+++ src/prm2t/Generator.java
@@ -6,16 +6,6 @@ import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
-//Klasa Generator:
-//
-//int dificulty - przechowuje poziom trudności czyli w zasadzie jak duża będzie plansza
-//
-//metody:
-//generateFromText - generuje planszę podaną przez użytkownika
-//generateRandom - generuje losową planszę zależną od wybranego poziomu trudności
-//setDificulty - ustawia poziom trudności
-//getDificulty - zwraca poziom trudności
-//checkFile - sprawdza czy istnieje ścieżka wczytywanego pliku
 public class Generator {
 
     private int dificulty = 0;
@@ -24,7 +14,7 @@ public class Generator {
     public Generator(int dificulty) {
         this.dificulty = dificulty;
     }
-
+    /* funkcja generujaca plansze z pliku txt */
     public void generateFromText(String path) throws IOException {
 
         int j = 0;
@@ -53,7 +43,7 @@ public class Generator {
         }
 
     }
-
+    /* pomocnicze gettery, możliwe że będą w przyszłości usunięte */
     public int getValueFromBoard(int coordinates) {
 
         Values v = board.get(coordinates);
@@ -75,34 +65,166 @@ public class Generator {
             return false;
         }
     }
-
+    /* funkcja generujaca losowa plansze na podstawie wbranego przez uzytkownika poziomu trudnosci */
     public void generateRandom(int dificulty) {
+        int boardSize = 0;
+        int blackSize = 0;
         if(dificulty==0) {
-            int boardSize = 25;
-            int blackSize = getRandomNumber(7,8);
+            boardSize = 25;
+            blackSize = getRandomNumber(7, 7);
+        }
+        if(dificulty==1) {
+            boardSize = 36;
+            blackSize = getRandomNumber(7, 7);
+        }
+        if(dificulty==2) {
+            boardSize = 49;
+            blackSize = getRandomNumber(7, 7);
+        }
             ArrayList<Integer> blackCoordinates = new ArrayList<>();
-            for(int i=0; i<blackSize; ++i) {
-                int tempCoordinates = getRandomNumber(0,boardSize-1);
-                if(blackCoordinates.contains(tempCoordinates)){
-                    --i;
+            ArrayList<Integer> whiteCoordinates = new ArrayList<>();
+            boolean result1 = false;
+            boolean result2 = false;
+
+            do {
+                blackCoordinates.clear();
+                whiteCoordinates.clear();
+                for (int i = 0; i < blackSize; ++i) {
+                    int tempCoordinates = getRandomNumber(0, boardSize - 1);
+                    if (blackCoordinates.contains(tempCoordinates)) {
+                        --i;
+                    } else {
+                        blackCoordinates.add(tempCoordinates);
+                    }
                 }
-                else{
-                    blackCoordinates.add(tempCoordinates);
+                for (int i = 0; i < boardSize; ++i) {
+                    if (!blackCoordinates.contains(i)) {
+                        whiteCoordinates.add(i);
+                    }
+
+                }
+                Collections.sort(blackCoordinates);
+                Collections.sort(whiteCoordinates);
+                result1 = checkNextBlack(boardSize, blackCoordinates);
+                result2 = checkNextWhite(whiteCoordinates);
+            } while(!result1 || !result2);
+//            System.out.println(whiteCoordinates);
+//            System.out.println(blackCoordinates);
+
+            for(int i=0; i<whiteCoordinates.size(); ++i) {
+                int width = (int)(Math.sqrt(boardSize));
+                int randomNumber = getRandomNumber(1,width);
+                //board.add(whiteCoordinates.get(i),new Values(randomNumber,false));
+                /* w tym miejscu pozostaje do zrobienia funkcja dodająca losowe cyfry w białe miejsca, bez powtorzen
+                * nastepnie w czarne miejsca z wystepujacymi juz cyframi w rzedzie lub wierszu  */
+            }
+        }
+
+    /* jeżeli true, plansza poprawna */
+    private boolean checkNextBlack(int boardsize, ArrayList<Integer> blackCoordinates) {
+        for(int i=0; i< blackCoordinates.size(); ++i) {;
+            if(!isThereBlackArea(boardsize, blackCoordinates.get(i), blackCoordinates)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    /* jeżeli true, plansza poprawna */
+    private boolean isThereBlackArea(int boardsize,int coordinates,ArrayList<Integer> blackCoordinates) {
+        int pixel = returnTop(coordinates);
+        if(pixel == -1) {
+            return true;
+        }
+        if(blackCoordinates.contains(pixel)) {
+            return false;
+        }
+
+        pixel = returnBottom(coordinates);
+        if(pixel == -1) {
+            return true;
+        }
+        if(blackCoordinates.contains(pixel)) {
+            return false;
+        }
+
+        pixel = returnNext(coordinates);
+        if(pixel == -1) {
+            return true;
+        }
+        if(blackCoordinates.contains(pixel)) {
+            return false;
+        }
+
+        pixel = returnPrevious(coordinates);
+        if(pixel == -1) {
+            return true;
+        }
+        if(blackCoordinates.contains(pixel)) {
+            return false;
+        }
+        return true;
+    }
+    /* jeżeli true, plansza poprawna */
+    private boolean checkNextWhite(ArrayList<Integer> whiteCoordinates) {
+        ArrayList<Integer> checkedWhitePixels = new ArrayList<>();
+        int seed = whiteCoordinates.get(0);
+        checkedWhitePixels.add(seed);
+        for(int i = 0; i<checkedWhitePixels.size(); ++i) {
+            if(whiteCoordinates.contains(returnNext(seed))) {
+                if(!checkedWhitePixels.contains(returnNext(seed))) {
+                    checkedWhitePixels.add(returnNext(seed));
                 }
             }
-            Collections.sort(blackCoordinates);
-            System.out.println(blackCoordinates);
+            if(whiteCoordinates.contains(returnPrevious(seed))) {
+                if(!checkedWhitePixels.contains(returnPrevious(seed))) {
+                    checkedWhitePixels.add(returnPrevious(seed));
+                }
+            }
+            if(whiteCoordinates.contains(returnTop(seed))) {
+                if(!checkedWhitePixels.contains(returnTop(seed))) {
+                    checkedWhitePixels.add(returnTop(seed));
+                }
+            }
+            if(whiteCoordinates.contains(returnBottom(seed))) {
+                if(!checkedWhitePixels.contains(returnBottom(seed))) {
+                    checkedWhitePixels.add(returnBottom(seed));
+                }
+            }
+            if(i+1<checkedWhitePixels.size()) {
+                seed = checkedWhitePixels.get(i+1);
+            }
         }
-        else if(dificulty==1){
-
+        if(checkedWhitePixels.size() == whiteCoordinates.size()) {
+            return true;
         }
-        else{
-
-        }
+        return false;
     }
 
-    public int getRandomNumber(int min, int max) {
+    private int getRandomNumber(int min, int max) {
         return ThreadLocalRandom.current().nextInt(min, max + 1);
     }
+
+    private int returnTop(int coordinates) {
+        return coordinates+5;
+    }
+
+    private int returnBottom(int coordinates) {
+        return coordinates-5;
+    }
+
+    private int returnPrevious(int coordinates) {
+        if(coordinates%5 == 0) {
+            return -1;
+        }
+        return coordinates-1;
+    }
+
+    private int returnNext(int coordinates) {
+        if((coordinates+1)%5 == 0) {
+            return -1;
+        }
+        return coordinates+1;
+    }
+
 
 }
--- src/prm2t/Main.java
+++ src/prm2t/Main.java
@@ -5,10 +5,8 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Generator generator = new Generator(0);
-        generator.generateFromText("resources/plansza.txt");
-//       System.out.println(generator.getValueFromBoard(24));
-//        System.out.println(generator.getColorFromBoard(24));
+        generator.generateFromText("resources\\plansza.txt");
         generator.generateRandom(0);
-       //System.out.println(generator.xxx(4,4));
+
     }
 }
