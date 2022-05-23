2c6340a3348bc31ce5050a347a3a3f3a99e694c8
flapicz <01169318@pw.edu.pl>
2022-05-23 16:44:35 +0200

    Aktualizacja metody saveBoard klasy Saver

--- src/prm2t/Main.java
+++ src/prm2t/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Generator generator = new Generator(0);
-        generator.generateFromText("D:\\studia\\programowanie\\JS\\Hitori_projekt\\prm2t22l_pro_kordziukiewicz_hitori\\resources\\plansza.txt");
+        generator.generateFromText("resources/plansza.txt");
 //       System.out.println(generator.getValueFromBoard(24));
 //        System.out.println(generator.getColorFromBoard(24));
         generator.generateRandom(0);
